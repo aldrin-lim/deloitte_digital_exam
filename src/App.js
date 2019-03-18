261e73d187503aaba11bd96b83406b68e284e478
Aldrin Lim <aldrinlim81@gmail.com>
2019-03-18 21:13:56 +0800

    done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 import header from './Assets/space-photo.jpeg';
 import Form from './Container/Form';
 import Mission from './Container/Mission';
+import ScrollableAnchor from 'react-scrollable-anchor'
 class App extends Component {
   state = {
     hide: false
@@ -13,16 +14,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className="main-continaer">
-          <div className="header">
-            <img src={header} />
-            <h1>Discover Space Missions</h1>
-            <a uk-icon="icon: chevron-down; ratio: 2" />
-          </div>
-          <div className="container">
-            <div className="uk-card uk-card-default uk-card-small uk-card-body">
-              <Mission />
+          <ScrollableAnchor id={'header'}>
+            <div className="header">
+              <img src={header} />
+              <h1>Discover Space Missions</h1>
+              <a id="headerButton" href="#container" uk-icon="icon: chevron-down; ratio: 2" />
             </div>
+          </ScrollableAnchor>
+          <ScrollableAnchor id={'container'}>
+            <div className="container">
+              <div className="uk-card uk-card-default uk-card-small uk-card-body">
+                <Mission />
+              </div>
+            </div>
+          </ScrollableAnchor>
+          <div className="footer">
+            <span>Copyright © 2018 Space Savvy  </span>
+            <a id="footerButton" href="#header">Back to Top</a>
           </div>
         </div>
       </div>
