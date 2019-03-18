57ed2e8dfdb73850788c176e136633b4f9c1e871
Aldrin Lim <aldrinlim81@gmail.com>
2019-03-18 21:25:23 +0800

    added buttons

--- src/Container/Mission/MissionList.js
+++ src/Container/Mission/MissionList.js
@@ -15,9 +15,14 @@ export default class MissionList extends Component {
             <h1>{props.rocket.rocket_name} - {props.payloads[0].payload_id} { props.launch_success === false && (<>- <span style={{color: "#ec5f7a"}}>Failed Mission</span></>) }</h1>
             <p>Launched at <b>{date.format('MMMM Do YYYY')}</b> at <b>{date.format('h:mma')}</b> from <b>{props.launch_site.full_name}</b></p>
             <div className="actions">
-              <button className="uk-button uk-button-default">Reddit Campaign</button>
-              <button className="uk-button uk-button-default">Reddit Launch</button>
-              <button className="uk-button uk-button-default">Reddit Media</button>
+              { props.links.reddit_campaign && <a target="_blank" href={props.links.reddit_campaign} className="uk-button uk-button-default">Reddit Campaign</a> }
+              { props.links.reddit_launch && <a target="_blank" href={props.links.reddit_launch} className="uk-button uk-button-default">Reddit Launch</a> }
+              { props.links.reddit_recovery && <a target="_blank"href={props.links.reddit_recovery} className="uk-button uk-button-default">Reddit Recovery</a> }
+              { props.links.reddit_media && <a target="_blank" href={props.links.reddit_media} className="uk-button uk-button-default">Reddit Media</a> }
+              { props.links.presskit && <a target="_blank" href={props.links.presskit} className="uk-button uk-button-default">Press Kit</a> }
+              { props.links.article_link && <a target="_blank" href={props.links.article_link} className="uk-button uk-button-default">Article</a> }
+              { props.links.video_link && <a target="_blank" href={props.links.video_link} className="uk-button uk-button-default">Watch Video</a> }
+              
             </div>
           </div>
           <div className="flight-number">
